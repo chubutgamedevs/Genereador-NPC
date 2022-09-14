f32cdaf59bda9e2d872eab79500b6f80fb47bc5b
unknown <gadiel068@gmail.com>
2022-09-14 12:56:53 -0300

    d

--- Assets/Scripts/gamemanager.cs
+++ Assets/Scripts/gamemanager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class gamemanager : MonoBehaviour
 {
@@ -8,23 +9,33 @@ public class gamemanager : MonoBehaviour
     public CharacterPower clon0;
     public CharacterPower clon1;
     public CharacterPower clon2;
-
-    public List<GameObject> sospechosos;
+    public CharacterPower clon3;
+    public CharacterPower clon4;
+    public CharacterPower clon5;
+    public CharacterPower clon6;
+    
+    public List<CharacterPower> sospechosos;
 
     private void Start()
     {
-        GameObject[] clon = GameObject.FindGameObjectsWithTag("clon");
+
+        // GameObject[] clon = GameObject.FindGameObjectsWithTag("clon");
+
+        //sospechosos = new List<CharacterPower>();
+        MutarYClonar();
+
     }
+
     public void MutarYClonar()
     {
+        sospechosos = sospechosos.OrderBy(_ => Random.value).ToList();
+        original = sospechosos[0];
         original.Randomize();
-        clon0.Clonar(original.adn);
-        clon0.Mutar(0);
 
-        clon1.Clonar(original.adn);
-        clon1.Mutar(1);
-
-        clon2.Clonar(original.adn);
-        clon2.Mutar(2);
+        for (int i = 1; i<sospechosos.Count; i++)
+        {
+            sospechosos[i].Clonar(original.adn);
+            sospechosos[i].Mutar(i%3);
+        }
     }
 }
