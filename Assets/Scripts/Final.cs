9a5890c3379cf4f8dbbe71b4187fb50e14b22b95
PCRGB <gdvestudios@gmail.com>
2022-10-21 10:50:24 -0300

    AnimTweens

--- Assets/Scripts/Final.cs
+++ Assets/Scripts/Final.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Final : MonoBehaviour
 {
     public NPCGenerator acusado;
     private bool carcel;
+    public TextMeshPro texto;
+    private string mensaje; 
     private void Start()
+    
     {
         acusado.Clonate(MainGameManager.GetInstance().acusado);
 
@@ -24,13 +28,18 @@ public class Final : MonoBehaviour
     public void Culpable()
     {
         Debug.Log("Encontraste al culpable, enhorabuena");
+        mensaje = "Encontraste al culpable, enhorabuena";
+        texto.text = mensaje;
     }
     public void Inocente()
     {
         Debug.Log("era un inoscente, mejor suerte la proxima :)");
+        mensaje = "era un inoscente, mejor suerte la proxima :)";
+        texto.text = mensaje;
     }
     public void Reset()
     {
         SceneManager.LoadScene("Mein");
     }
+    
 }
