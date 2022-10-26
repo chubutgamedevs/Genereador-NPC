eddbca4d6f2a22949be24fce589c4139a208177c
Rodrigo <rodrigo.renecura@gmail.com>
2022-10-26 16:23:58 -0300

    Corrección de respiración anim. Acomodo de la arquitectura.

---
+++ Assets/Anims/RespiracionAnim.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using DG.Tweening;
+
+public class RespiracionAnim : MonoBehaviour
+{
+    public Vector3 amplitud = new Vector3(0,0.1f,0); // (X,Y,Z)
+    private Tweener tween;
+    
+    private void Start(){
+        tween = transform
+            .DOScale(transform.localScale + amplitud, Random.Range(3.0f ,5.0f))
+            .SetLoops(-1, LoopType.Yoyo) // Yoyo hace que la animación vaya y vuelva.
+            .SetDelay(Random.Range(0.0f, 1.0f));
+    }
+
+    private void OnDestroy(){
+        tween.Kill(); // Elimina el tween cuando el objeto no existe más.
+    }
+}
