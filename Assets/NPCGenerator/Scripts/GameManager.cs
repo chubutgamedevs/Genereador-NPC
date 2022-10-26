af307870cd91a672624ac72374f1348690bbbace
Rodrigo <rodrigo.renecura@gmail.com>
2022-10-26 15:57:47 -0300

    Agregadas imagenes de bocas y tops

--- Assets/NPCGenerator/Scripts/GameManager.cs
+++ Assets/NPCGenerator/Scripts/GameManager.cs
@@ -4,22 +4,30 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] List<NPCGenerator> npcs;
+    [SerializeField] GameObject npcs;
+    private List<NPCGenerator> _npcs;
+
+    private void Awake(){
+        _npcs = new List<NPCGenerator>(npcs.GetComponentsInChildren<NPCGenerator>());
+        Debug.Log("Wumpuses recolectados" + _npcs.Count);
+    }
+
 
     private void Start()
     {
+        
         GenerateNPCs();
     }
 
-    public void GenerateNPCs() => npcs.ForEach(npc => npc.Generate());
+    public void GenerateNPCs() => _npcs.ForEach(npc => npc.Generate());
 
     public void Variantes() {
-        npcs[0].Generate();
+        _npcs[0].Generate();
 
-        for (int i = 1; i < npcs.Count; i++)
+        for (int i = 1; i < _npcs.Count; i++)
         {
-            npcs[i].Clonate(npcs[0]);
-            npcs[i].Mutate(i);
+            _npcs[i].Clonate(_npcs[0]);
+            _npcs[i].Mutate(i);
         }
     }
 }
--- Assets/Scripts/WumpusVivo.cs
+++ Assets/Scripts/WumpusVivo.cs
@@ -1,45 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 
-
 public class WumpusVivo : MonoBehaviour
 {
-    public Vector3 respirar; // (X,Y,Z)
-    private Vector3 normal;
-    private float agitado;
-
-
+    public Vector3 respirar = new Vector3(0,0.1f,0); // (X,Y,Z)
+    
     private void Start(){
-        respirar = new Vector3(0,0.1f,0);
-        normal =  transform.localScale;
-        StartCoroutine(StartTween());
-        agitado = Random.Range(2.0f ,2.5f);
+        Vector3 normal =  transform.localScale;
+        float agitado = Random.Range(3.0f ,5.0f);
+
+        Sequence s = DOTween.Sequence();
+        s.Append(transform.DOScale(normal+respirar, agitado));
+        s.Append(transform.DOScale(normal, agitado));
+        s.SetLoops(-1);
+        s.SetDelay(Random.Range(0.0f, 1.0f));
     }
-    IEnumerator StartTween()
-    {
-        Tween initialTween = gameObject.transform.DOScale(normal + respirar, 1f).SetDelay(Random.Range(0,1.3f));
-
-        yield return initialTween.WaitForCompletion();
-
-        DOTween
-            .Sequence()
-            .Append
-            (
-                gameObject.transform.DOScale(normal+respirar, agitado)
-            )
-            .Append
-            (
-                gameObject.transform.DOScale(normal, agitado)
-            )
-            .Append
-            (
-                gameObject.transform.DOScale(normal+respirar, agitado)
-            )
-            .SetLoops(-1);
-
-        yield return 0f;
-    }
-
 }
